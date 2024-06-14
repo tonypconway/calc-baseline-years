50b1afbcbb9a07fcac3767db4b5241d016666a20
Tony Conway <tony.p.conway@gmail.com>
2024-06-14 17:43:07 +0100

    Simplify to MDN data only, add readme and license.

---
+++ README.md
@@ -0,0 +1,76 @@
+# `calc-baseline-years`
+
+## What is this module?
+
+This module provides a few simple functions for obtaining minimum browser version numbers and release dates for each version of the Web Platform Baseline.
+
+## Functions
+
+There are three functions available in this module.
+
+1. `getAllMinBaselineVersions(?year)`
+2. `getMinBaselineVersionsByYear(year)`
+3. `getMinBaseilneVersionByBrowserAndYear(browser, year)`
+
+### `getAllMinBaselineVersions()`
+
+Returns a Javascript `Object` with the following format representing Baseline minimum browser versions for the year in question:
+
+``` json
+{
+  "2016": {
+    "chrome": {
+      "version": "47",
+      "release_date": "2015-12-01"
+    },
+    "chrome_android": {
+      "version": "47",
+      "release_date": "2015-12-02"
+    },
+    "edge": {
+      "version": "13",
+      "release_date": "2015-11-12"
+    },
+    "firefox": {
+      "version": "43",
+      "release_date": "2015-12-15"
+    },
+    "firefox_android": {
+      "version": "43",
+      "release_date": "2015-12-15"
+    },
+    "safari": {
+      "version": "9",
+      "release_date": "2015-09-30"
+    },
+    "safari_ios": {
+      "version": "9",
+      "release_date": "2015-09-16"
+    }
+  },
+  ...
+}
+```
+This function takes a single argument, `year`, which is an int or string in the format `YYYY`.  If you pass a number lower than 2004, the script will throw an error, as none of the Baseline browser set existed before 2003.
+
+### `getMinBaselineVersionsByYear(year)`
+
+This function takes ones argument, `year`.
+
+* `browser` should be a string from the following list:
+  * `chrome`
+  * `chrome_android`
+  * `edge`
+  * `firefox`
+  * `firefox_android`
+  * `safari`
+  * `safari_ios`
+* `year` should be a four digit representation of a year between 2016 and the current year.
+
+The function will return the object corresponding to the selected year in the same format as `getAllBaselineVersions()`.
+
+### `getMinBaseilneVersionByBrowserAndYear(browser, year)`
+
+This function takes two arguments: `browser` and `year`.
+
+This function returns an object containing the `version` and `release_date` objects for the requested browser and year.
---
+++ index.js
@@ -0,0 +1,92 @@
+import bcd from '@mdn/browser-compat-data' assert { type: 'json' };
+
+// https://github.com/web-platform-dx/web-features/blob/main/docs/baseline.md#core-browser-set
+const browsers = [
+  "chrome",
+  "chrome_android",
+  "edge",
+  "firefox",
+  "firefox_android",
+  "safari",
+  "safari_ios",
+];
+
+// Earliest release year for each of the Baseline browsers
+const browsersFirstYear = {
+  "chrome": 2008,
+  "chrome_android": 2012,
+  "edge": 2015,
+  "firefox":2004,
+  "firefox_android": 2011,
+  "safari": 2003,
+  "safari_ios": 2007
+}
+
+export function getMinBaselineVersionByBrowserAndYear(browser, year) {
+  
+    let arrayOfVersions = new Array();
+
+    Object.entries(
+      Object.fromEntries(
+        Object.entries(bcd.browsers[browser].releases).filter(
+          ([version, data]) => {
+            if (!['current', 'esr', 'retired'].includes(data.status)) {
+              return false;
+            }
+            if (!data.release_date.startsWith(`${year-1}-`)) {
+              return false;
+            }
+            return true;
+          }
+        )
+      )
+    ).forEach(data => {
+      arrayOfVersions.push({ version: data[0], release_date: data[1].release_date });
+    }, 0);
+
+    if (arrayOfVersions.length != 0) {
+      return arrayOfVersions.sort((a, b) => Date.parse(a.release_date) - Date.parse(b.release_date)).pop();
+    } else if (year >= browsersFirstYear[browser]) {
+      return getMinBaselineVersionByBrowserAndYear(browser, year-1)
+    } else {
+      return null
+    }
+}
+
+export function getMinBaselineVersionsByYear(year) {
+
+  if (year < 2004) {
+    throw("There are no compatible Baseline browser versions before 2004!")
+  }
+
+  let versionsByYear = {}
+
+  browsers.forEach(browser => {
+
+    versionsByYear[browser] = {}
+
+    let finalVersion = getMinBaselineVersionByBrowserAndYear(browser, year);
+
+    versionsByYear[browser] = 
+      finalVersion != null
+        ? finalVersion
+        : getMinBaselineVersionByBrowserAndYear(browser, year-1);
+  });
+
+  return versionsByYear;
+
+}
+
+export function getAllMinBaselineVersions(startingYear = 2016) {
+
+  const currentYear = new Date().getFullYear();
+  const finalVersionsPerYear = {};
+
+  for (let year = startingYear; year <= currentYear; year++) {
+
+    finalVersionsPerYear[year] = getMinBaselineVersionsByYear(year);
+
+  }
+
+  return finalVersionsPerYear;
+};
